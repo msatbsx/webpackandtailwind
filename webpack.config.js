44c5e7c4285d06311a1b57fef1e7bf3041705433
Michal S <ms@bsx.pl>
2019-11-12 13:55:43 +0800

    assets loading added

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV,
@@ -11,16 +12,31 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Webapack&Tailwind starter',
     }),
+    new CopyWebpackPlugin([
+      {from: 'src/assets', to: 'assets'},
+    ])
   ],
   output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
       },
     ]
   }
