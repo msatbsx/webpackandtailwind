91b45679cd57dd65a0bc063eac504d06b952f1e5
Michal <ms@bsx.pl>
2022-09-07 14:29:17 +0800

    updated to reflect September 22 state of dependencies

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ function sampleComponent() {
   firstInnerElement.appendChild(secondInnerElement);
 
   const firstP = document.createElement('p');
-  firstP.classList.add('font-semibold', 'text-lg', 'mb-1', 'text-gray-900');
+  firstP.classList.add('font-semibold', 'text-lg', 'mb-1', 'text-amber-700');
   firstP.innerHTML = 'Project starter';
   secondInnerElement.appendChild(firstP);
 
   const secondP = document.createElement('p');
-  secondP.classList.add('text-gray-700', 'mb-2');
+  secondP.classList.add('text-green-600', 'mb-2');
   secondP.innerHTML = 'Let\'s create something amazing.';
   secondInnerElement.appendChild(secondP);
 
